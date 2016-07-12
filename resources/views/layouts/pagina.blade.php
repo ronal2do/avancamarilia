9cd7d75ebbb7156eb6cb323f867b6268a9783c5f
System Administrator <root@SOTMAC14.local>
2016-07-12 14:17:12 -0300

    svg home

--- resources/views/layouts/pagina.blade.php
+++ resources/views/layouts/pagina.blade.php
@@ -10,9 +10,12 @@
 	     </div>
 	     <div class="col-md-10 col-md-offset-1 Padding">
 	       @yield('texto')
+	       <div class="v"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="45px" height="35px" viewBox="0 0 45 35" xml:space="preserve"><path style="fill:#F9D543;" d="M0,11.639h8.494l4.409,12.741L21.757,0h8.364L16.469,34.495H9.207L0,11.639z"/></svg></div>
+	       <br><br>
 	     </div>
 	     <div class="col-md-10 col-md-offset-1 Amarelo-link" style="">
-	       <a class="" href="/menu">	<p>< Voltar </p></a>
+	       <a class="" href="/menu">	<p><i class="fa fa-play fa-rotate-180"></i> Voltar </p></a>
 	     </div>
     </div>
 </div><!-- final do página colocar o V -->
