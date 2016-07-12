0bd702aa1a41bfc379bed3fd2e51abc04c7cc340
System Administrator <root@SOTMAC14.local>
2016-07-12 18:40:27 -0300

    algumas mudancas

--- resources/views/layouts/pagina.blade.php
+++ resources/views/layouts/pagina.blade.php
@@ -17,8 +17,7 @@
 
 	     <div class="col-md-8 col-md-offset-2 Padding">
 	       @yield('texto')
-	       <div class="v"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 width="45px" height="35px" viewBox="0 0 45 35" xml:space="preserve"><path style="fill:#F9D543;" d="M0,11.639h8.494l4.409,12.741L21.757,0h8.364L16.469,34.495H9.207L0,11.639z"/></svg></div>
+
 	       <br><br>
 
 	        <a style="color: #192854;" href="/menu">	<p><i class="fa fa-play fa-rotate-180"></i> Voltar </p></a>
--- resources/views/pages/menu.blade.php
+++ resources/views/pages/menu.blade.php
@@ -5,40 +5,52 @@
  <div class="Home">
      <div class="col-md-10 col-md-offset-1">
        <div class="container Menu">
-       	<div class="Menu-item waves">
+       	<div class="Menu-item waves Flex">
        		<a href="/cidade">
-       		<div class="Menu-texto">Avança no cuidado com a cidade</div>
-       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
+             		<div class="Menu-texto">Avança no cuidado com a cidade</div>
+                  </a>  
+                  <a href="/cidade" class="Menu-btn">
+                        <div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
-       	<div class="Menu-item waves">
+       	<div class="Menu-item waves Flex">
        		<a href="/pessoas">
-       		<div class="Menu-texto">Avança na atenção com as pessoas</div>
-       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
+             		<div class="Menu-texto">Avança na atenção com as pessoas</div>
+                  </a> 
+                  <a href="/pessoas" class="Menu-btn">
+                        <div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
-       	<div class="Menu-item waves">
+       	<div class="Menu-item waves Flex">
        		<a href="/oportunidades">
-       		<div class="Menu-texto">Avança na geração de oportunidades</div>
-       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
+             		<div class="Menu-texto">Avança na geração de oportunidades</div>
+                  </a> 
+                  <a href="/oportunidades" class="Menu-btn">
+                        <div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
-       	<div class="Menu-item waves">
+       	<div class="Menu-item waves Flex">
        		<a href="/bem-estar">
-       		<div class="Menu-texto">Avança na promoção do bem-estar</div>
-       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
+             		<div class="Menu-texto">Avança na promoção do bem-estar</div>
+                  </a> 
+                  <a href="/bem-estar" class="Menu-btn">
+                        <div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
-       	<div class="Menu-item waves" >
+       	<div class="Menu-item waves Flex" >
        		<a href="/transparencia">
-       		<div class="Menu-texto">Avança na eficiência<br> e na tranparência</div>
-       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
+             		<div class="Menu-texto">Avança na eficiência<br> e na tranparência</div>
+             	</a>
+                  <a href="/transparencia" class="Menu-btn">
+                        <div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
-       	<div class="Menu-item waves">
-       		<a href="/sugestao">
-       		<div class="Menu-texto">Sugestão <i class="fa fa-play"></i></div>
-  <!--      		<div class="Menu-btn"><i class="fa fa-play"></i></div> -->
+       	<div class="Menu-item waves Flex">
+       		<a href="/sugestao" class="Menu-texto">
+             		<div class="Menu-texto">Sugestão <span style="visibility: hidden;">Sugestão Sugestão</span><br></div>
+                  </a> 
+                  <a href="/sugestao" class="Menu-btn">
+                        <div>    <i class="fa fa-play"></i></div>
        		</a>
 		</div>
        
