4aa3f591842438ffe76fc556f23e8ef73911db51
System Administrator <root@SOTMAC14.local>
2016-07-12 13:21:02 -0300

    mods menu 2

--- resources/views/pages/menu.blade.php
+++ resources/views/pages/menu.blade.php
@@ -7,38 +7,38 @@
        <div class="Menu">
        	<div class="Menu-item waves">
        		<a href="/cidade">
-       		<div class="Menu-texto">Avança no cuidado com a cidade <i class="fa fa-play"></i></div>
-<!--        		<div class="Menu-btn"><i class="fa fa-play"></i></div> -->
+       		<div class="Menu-texto">Avança no cuidado com a cidade</div>
+       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
        	<div class="Menu-item waves">
        		<a href="/pessoas">
-       		<div class="Menu-texto">Avança na atenção com as pessoas <i class="fa fa-play"></i></div>
-<!--        		<div class="Menu-btn"><i class="fa fa-play"></i></div> -->
+       		<div class="Menu-texto">Avança na atenção com as pessoas</div>
+       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
        	<div class="Menu-item waves">
        		<a href="/oportunidades">
-       		<div class="Menu-texto">Avança na geração de oportunidades <i class="fa fa-play"></i></div>
-<!--        		<div class="Menu-btn"><i class="fa fa-play"></i></div> -->
+       		<div class="Menu-texto">Avança na geração de oportunidades</div>
+       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
        	<div class="Menu-item waves">
        		<a href="/bem-estar">
-       		<div class="Menu-texto">Avança na promoção do bem-estar <i class="fa fa-play"></i></div>
-<!--        		<div class="Menu-btn"><i class="fa fa-play"></i></div> -->
+       		<div class="Menu-texto">Avança na promoção do bem-estar</div>
+       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
        	<div class="Menu-item waves" >
        		<a href="/transparencia">
-       		<div class="Menu-texto">Avança na eficiencia e na tranparência <i class="fa fa-play"></i></div>
-<!--        		<div class="Menu-btn"><i class="fa fa-play"></i></div> -->
+       		<div class="Menu-texto">Avança na eficiencia<br> e na tranparência</div>
+       		<div class="Menu-btn"><i class="fa fa-play"></i></div>
        		</a>
 		</div>
        	<div class="Menu-item waves">
        		<a href="/sugestao">
        		<div class="Menu-texto">Sugestão <i class="fa fa-play"></i></div>
-<!--        		<div class="Menu-btn"><i class="fa fa-play"></i></div> -->
+  <!--      		<div class="Menu-btn"><i class="fa fa-play"></i></div> -->
        		</a>
 		</div>
        
